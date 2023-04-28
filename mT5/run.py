c7af8389e3b8127d7972483301b9691ead65b498
Pakapak <44494218+amejiyo@users.noreply.github.com>
2023-04-28 21:43:46 +0700

    add web app

--- mT5/run.py
+++ mT5/run.py
@@ -1,15 +1,16 @@
 import re
+import os
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 # pip install transformers
 import warnings
-
 warnings.filterwarnings("ignore")
+
 WHITESPACE_HANDLER = lambda k: re.sub('\s+', ' ', re.sub('\n+', ' ', k.strip()))
 
 article_text = ""
-file_path="/Users/amejiyopakapak/Documents/NLP/Project/webapp/data/input_text.txt"     # input text file
+file_path="/Users/amejiyopakapak/Documents/NLP/Web2TextSum/webapp/data"     # input text file
 
-with open(file_path, "r", encoding="UTF-8") as file:
+with open(os.path.join(file_path, "input_text.txt"), "r", encoding="UTF-8") as file:
     article_text = (" ").join([line.rstrip() for line in file])
 
 model_name = "csebuetnlp/mT5_multilingual_XLSum"
@@ -37,4 +38,7 @@ summary = tokenizer.decode(
     clean_up_tokenization_spaces=False
 )
 
-print(summary)
+
+with open(os.path.join(file_path, "output_text.txt"), "w", encoding="UTF-8") as file:
+    file.write(summary)
+file.close()
---
+++ webapp/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <div className='WebApp-left'>
+                this is left
+            </div>
+            <div className='WebApp-right'>
+                this is right
+            </div>
+        </div>
+    );
+  }
+
+  export default App;
